c7327eb59b6d8e767fcdf3f93b299f0334aa6305
Nawaf Alolayan <Nawafaaa0@gmail.com>
2026-04-30 17:29:40 +0300

    initial commit - MAEDIN app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maedin.app',
+  appName: 'MAEDIN',
+  webDir: 'client/dist'
+};
+
+export default config;
