45ea1e7ca2e8168ea8a87233851f0ffdcef47b19
nawafoly <114729674+nawafoly@users.noreply.github.com>
2026-09-06 14:27:20 +0300

    fix(habat): make Maedin-aligned attendance app the Habbat entrypoint

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
-import HabatAttendanceApp from "./pages/habat/HabatAttendanceApp";
+import HabatAttendanceApp from "./pages/habat/HabatAttendanceAppV3";
 import { initializeDocumentLanguage } from "./contexts/LanguageContext";
 import "./index.css";
 
